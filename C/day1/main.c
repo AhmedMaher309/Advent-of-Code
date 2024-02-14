954e17f299067585cc97b121133fcb568331a265
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-02-15 01:05:25 +0200

    update main.c

--- C/day1/main.c
+++ C/day1/main.c
@@ -5,62 +5,43 @@
 
 #define MAX_LINE_LENGTH 100
 
-char * allToDigits(char * str){
-  char* digitMap[] = {
-        "one", "1",
-        "two", "2",
-        "three", "3",
-        "four", "4",
-        "five", "5",
-        "six", "6",
-        "seven", "7",
-        "eight", "8",
-        "nine", "9"
-    };
-  int numMappings = sizeof(digitMap) / sizeof(*digitMap);
-  size_t inputLength = strlen(str);
-  char* result = (char*)malloc((inputLength + 1) * sizeof(char));
-  if (result == NULL) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
-  }
-  result[0] = '\0';
-  char * ptr = str;
+char *twodigits(char *str) {
+  char *ptr = str;
+  char firstdigit = '\0';
+  char lastdigit = '\0';
+  int count = 0;
+  char *digitMap[] = {"one",   "1", "two",   "2", "three", "3",
+                      "four",  "4", "five",  "5", "six",   "6",
+                      "seven", "7", "eight", "8", "nine",  "9"};
 
   while (*ptr != '\0') {
-        int foundMapping = 0;
-        for (int j = 0; j < numMappings; j += 2) {
-            char* match = strstr(ptr, digitMap[j]);
-            if (match == ptr) {
-                strcat(result, digitMap[j + 1]);
-                foundMapping = 1;
-                ptr += strlen(digitMap[j]);
-                break;
-            }
+    if (isdigit(*ptr) && count == 0) {
+      firstdigit = *ptr;
+      lastdigit = firstdigit;
+      count++;
+    } else if (isdigit(*ptr) && count != 0) {
+      lastdigit = *ptr;
+    } else {
+      for (int i = 0; i < 18; i += 2) {
+        char *match = strstr(ptr, digitMap[i]);
+        if (match == ptr) {
+          if (count == 0) {
+            firstdigit = *digitMap[i + 1];
+            lastdigit = *digitMap[i + 1];
+          } else {
+            lastdigit = *digitMap[i + 1];
+          }
         }
-        if (!foundMapping) {
-            strncat(result, ptr, 1);
-            ptr++;
-        }
+      }
     }
-    return result;
-
-} 
-
-char* extractNums(char * str){
-  size_t inputLength = strlen(str);
-  char* result = (char*)malloc((inputLength + 1) * sizeof(char));
-  for(int i = 0; i < strlen(str); i++){
-    if(isdigit(str[i])){
-      char temp[2];
-      temp[0] = str[i];
-      temp[1] = '\0';
-      strcat(result, temp);
-    }
+    ptr++;
   }
+  char *result = (char *)malloc(2 * sizeof(char));
+  result[0] = firstdigit;
+  result[1] = lastdigit;
+
   return result;
 }
-
 
 int main(void) {
   int totalSum = 0;
@@ -73,15 +54,13 @@ int main(void) {
     printf("Error: could not open input file\n");
     return EXIT_FAILURE;
   }
- 
-   while (fgets(line, sizeof(line), fp) != NULL) {
-     char* str = line;
-     char * string  = allToDigits(str);
-     char * nums = extractNums(string);
-     int smallSum = ((nums[0] - '0') * 10) + (nums[strlen(nums) - 1] - '0');
-     totalSum += smallSum;
-    }
 
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    char *str = line;
+    char *result = twodigits(str);
+    int sum = ((result[0] -'0') * 10) + (result[1] - '0');
+    totalSum += sum;
+  }
 
   printf("Final sum is %d\n", totalSum);
   fclose(fp);
