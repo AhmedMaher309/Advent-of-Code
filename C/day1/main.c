de2abb8bca68b14fc7a6b34820511f7306c5bf7e
AhmedMaher309 <76390783+AhmedMaher309@users.noreply.github.com>
2024-02-14 02:16:42 +0200

    updated main.c

--- C/day1/main.c
+++ C/day1/main.c
@@ -1,43 +1,85 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
+char * allToDigits(char * str){
+  char* digitMap[] = {
+        "one", "1",
+        "two", "2",
+        "three", "3",
+        "four", "4",
+        "five", "5",
+        "six", "6",
+        "seven", "7",
+        "eight", "8",
+        "nine", "9"
+    };
+  int numMappings = sizeof(digitMap) / sizeof(*digitMap);
+  size_t inputLength = strlen(str);
+  char* result = (char*)malloc((inputLength + 1) * sizeof(char));
+  if (result == NULL) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+  }
+  result[0] = '\0';
+  char * ptr = str;
+
+  while (*ptr != '\0') {
+        int foundMapping = 0;
+        for (int j = 0; j < numMappings; j += 2) {
+            char* match = strstr(ptr, digitMap[j]);
+            if (match == ptr) {
+                strcat(result, digitMap[j + 1]);
+                foundMapping = 1;
+                ptr += strlen(digitMap[j]);
+                break;
+            }
+        }
+        if (!foundMapping) {
+            strncat(result, ptr, 1);
+            ptr++;
+        }
+    }
+    return result;
+
+} 
+
+char* extractNums(char * str){
+  size_t inputLength = strlen(str);
+  char* result = (char*)malloc((inputLength + 1) * sizeof(char));
+  for(int i = 0; i < strlen(str); i++){
+    if(isdigit(str[i])){
+      char temp[2];
+      temp[0] = str[i];
+      temp[1] = '\0';
+      strcat(result, temp);
+    }
+  }
+  return result;
+}
 int main(void) {
   int totalSum = 0;
   char *filename = "input.txt";
   FILE *fp = fopen(filename, "r");
 
+  char line[MAX_LINE_LENGTH];
+
   if (fp == NULL) {
     printf("Error: could not open input file\n");
     return EXIT_FAILURE;
   }
-  printf("Reading the file...\n");
-  char ch;
-  int lineNumber = 0;
-  int sum;
-  char *digits[] = {"one", "two",   "three", "four", "five",
-                    "six", "seven", "eight", "nine"};
-  while ((ch = fgetc(fp)) != EOF) {
-    int count = 0;
-    char lastNum = '\0';
-    while (ch != '\n') {
-      if (isdigit(ch)) {
-        if (count == 0) {
-          sum = ch - '0';
-        }
-        count++;
-        lastNum = ch;
-      }
-      ch = fgetc(fp);
+ 
+   while (fgets(line, sizeof(line), fp) != NULL) {
+     char* str = line;
+     char * string  = allToDigits(str);
+     char * nums = extractNums(string);
+     int smallSum = ((nums[0] - '0') * 10) + (nums[strlen(nums) - 1] - '0');
+     totalSum += smallSum;
     }
-    if (count > 0) {
-      sum = sum * 10 + (lastNum - '0');
-      totalSum += sum;
-      lineNumber++;
-      printf("%d and line is %d\n", sum, lineNumber);
-      ch = fgetc(fp);
-    }
-  }
+
 
   printf("Final sum is %d\n", totalSum);
   fclose(fp);
